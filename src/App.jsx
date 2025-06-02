29665fce9f0a1d24f1fbaa4852cb3997248505a0
Muneeba 29 <ibnehawwa29@gmail.com>
2025-06-02 18:47:37 +0500

    Your message about what changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-
 import './App.css';
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Navbar from './component/navbar';
+import Navbar from './components/Navbar';
 
- function App() {
+function App() {
   return (
-    <BrowserRouter basename='/react-project/'>
-    <Navbar />
+    <HashRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
-export default App
+export default App;
